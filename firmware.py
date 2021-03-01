1a602af60e9e7b5cc19837cf80bff47ca877e66e
Kelvin Henrique de Brito <kelvin.henrique@jtip.com.br>
2021-03-01 11:02:15 -0300

    bluetooth working?

--- firmware.py
+++ firmware.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-__VERSION__ = 1.4
+__VERSION__ = 1.5
 
 import time
 import pigpio
@@ -9,7 +9,7 @@ import bluetooth
 from datetime import datetime
 
 Debug = 1
-started = 0
+started = "0"
 Serial="21001" #Numero Serial do produto
 path = "/mnt/usb_share/logs/"
 
@@ -17,73 +17,94 @@ now = datetime.now()
 date = now.strftime("%d%m%y%H%M%S")
 csvname =path + date + "-" + Serial +".csv"
 
-server_socket=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
- 
-port = 1
-server_socket.bind(("",port))
-server_socket.listen(1)
+host = ""
+port = 1        # Raspberry Pi uses port 1 for Bluetooth Communication
+# Creaitng Socket Bluetooth RFCOMM communication
+server = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+print('Bluetooth Socket Created')
+try:
+        server.bind((host, port))
+        print("Bluetooth Binding Completed")
+except:
+        print("Bluetooth Binding Failed")
+server.listen(1) # One connection at a time
+# Server accepts the clients request and assigns a mac address.
+client, address = server.accept()
+print("Connected To", address)
+print("Client:", client)
 
-def getvalue(): #Comunica com I2C e retorna valores de press?o e temperatura
+def main():
+    try:
+        while True:
+            # Receivng the data.
+            data = client.recv(1024) # 1024 is the buffer size.
+            print(data)
 
-    pi = pigpio.pi()
-    h = pi.i2c_open(1, 0x78)
-    pi.i2c_write_device(h, [0xAC])
-    time.sleep(0.2)
-    (b, d) = pi.i2c_read_device(h, 6)
-    pi.i2c_close(h)
-    pi.stop()
+            if data == "1":
+                    print('recebido 1, iniciando medicao')
+                    started = "1"
+                    send_data = "iniciando medicao"
+            elif data == "0":
+                    print('recebido 0, parando medicao')
+                    started = "0"
+                    send_data = "parando medicao"
+            elif data != "0" and data != "1":
+                    send_data = "envie 1 ou 0 "
+            # Sending the data.
+            client.send(send_data)              
+            if started == "1":
 
-    p1 = bin(d[1])[2:].zfill(8) #BridgeDat1
-    p2 = bin(d[2])[2:].zfill(8) #BridgeDat2
-    p3 = bin(d[3])[2:].zfill(8) #BridgeDat3
-    t1 = bin(d[4])[2:].zfill(8) #TempDat1
-    t2 = bin(d[5])[2:].zfill(8) #TempDat2
-    press = p1+p2+p3
-    dpress = int(press, 2)
-    Pressure = (((((dpress/12305550)*100)*6)/100)-2.2)*1.042
-    temp = t1+t2
-    dtemp = int(temp, 2)
-    Temperature = (((dtemp/65536)*190)-40)*0.954
+                pi = pigpio.pi()
+                h = pi.i2c_open(1, 0x78)
+                pi.i2c_write_device(h, [0xAC])
+                time.sleep(0.2)
+                (b, d) = pi.i2c_read_device(h, 6)
+                pi.i2c_close(h)
+                pi.stop()
 
-    if Debug > 0:
-        print()
-        if Pressure < -1 or Pressure > 5:
-            print("out of range")
-        else:
-            print("Pressao = ", Pressure, "bar")
+                p1 = bin(d[1])[2:].zfill(8) #BridgeDat1
+                p2 = bin(d[2])[2:].zfill(8) #BridgeDat2
+                p3 = bin(d[3])[2:].zfill(8) #BridgeDat3
+                t1 = bin(d[4])[2:].zfill(8) #TempDat1
+                t2 = bin(d[5])[2:].zfill(8) #TempDat2
+                press = p1+p2+p3
+                dpress = int(press, 2)
+                Pressure = (((((dpress/12305550)*100)*6)/100)-2.2)*1.042
+                temp = t1+t2
+                dtemp = int(temp, 2)
+                Temperature = (((dtemp/65536)*190)-40)*0.954
 
-        if Temperature <0 or Temperature >100:
-            print("out of range")
-        else:
-            print("Temperatura = ", Temperature, "C")
-        #print("medicoes = ", contador)
+                if Debug > 0:
+                    print()
+                    if Pressure < -1 or Pressure > 5:
+                        print("out of range")
+                    else:
+                        print("Pressao = ", Pressure, "bar")
 
-    with open(csvname, mode='a') as sensor_readings:
-        sensor_write = csv.writer(sensor_readings, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        write_to_log = sensor_write.writerow([Pressure,Temperature])
-        return(write_to_log)
-    
+                    if Temperature <0 or Temperature >100:
+                        print("out of range")
+                    else:
+                        print("Temperatura = ", Temperature, "C")
+                        #print("medicoes = ", contador)
 
-print ("waiting for some bluetooth communication")    
-client_socket,address = server_socket.accept()
-print ("Accepted connection from ",address)
+                with open(csvname, mode='a') as sensor_readings:
+                    sensor_write = csv.writer(sensor_readings, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                    write_to_log = sensor_write.writerow([Pressure,Temperature])
+                    return(write_to_log)
 
-while True:
- 
-    data = client_socket.recv(1024)
-    print ("Received: %s" % data)
-    if (data == "1"):    #if '1' is sent from the Android App, start i2c def
-        print ("doing code 1 - Starting to get i2c data")
-        started = 1
-    if (data == "0"):    #if '1' is sent from the Android App, stop i2c def
-        print ("doing code 0 - Stopping to get i2c data")
-        started = 0
-        #code to stop?
-    if (data == "q"):
-        print ("doing code q - quiting program")
-        break    
-    if started == 1:
-        getvalue()
+                send_data = Pressure
+                client.send(send_data)
+            #else:
+               # print("started é diferente de 0, entao nada e feito")
 
-client_socket.close()
-server_socket.close()
+    except:
+        # Closing the client and server connection
+        client.close()
+        server.close()
+
+if __name__ == "__main__":
+    main()
+
+
+
+
